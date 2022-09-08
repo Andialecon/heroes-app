9bd6d6f1e300059141cbc956ef33a316281783e1
Mauro Alemán <andresalemanc@gmail.com>
2022-09-07 22:03:56 -0500

    configuracion de tailwind, estructura de carpetas y el  enrutamiento

---
+++ src/HeroesApp.jsx
@@ -0,0 +1,16 @@
+import { DcPage } from "./heroes/pages/DcPage"
+import { MarvelPage } from "./heroes/pages/MarvelPage"
+import { AppRouter } from "./router/AppRouter"
+
+
+export const HeroesApp = () => {
+
+    return (
+        <>
+
+            <AppRouter />
+
+        </>
+    )
+
+} 
