f064052ab5a285e3a9270d9433fb372025ea77d7
Mauro Alemán <mauro.aleman@numrot.com>
2022-09-08 07:59:03 -0500

    Navbar y archivos de barril - v.5.4

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,7 +1,8 @@
+import { Navbar } from "../../design/components/navbar/Navbar"
 
 export const DcPage = () => {
     return (
-        <>
+        <>  
             <h1>DC Page</h1>
         </>
     )
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
+import { Navbar } from "../design/components/navbar/Navbar";
 import { DcPage } from "../heroes/pages/DcPage";
 import { MarvelPage } from "../heroes/pages/MarvelPage";
 
 export const AppRouter = () => {
     return (
-        <>
-            
+        <>  
+            <Navbar />
             <Routes>
                 <Route path="/" element={ <LoginPage /> } />
                 <Route path="marvel" element={ <MarvelPage /> } />
